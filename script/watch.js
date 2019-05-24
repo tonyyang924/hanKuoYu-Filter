9788ef38557bc5e98c534083d85979c139f8a527
Tony Yang <superzoro168@gmail.com>
2019-05-24 14:54:40 +0800

    add some keywords to blocklist

--- script/watch.js
+++ script/watch.js
@@ -1,7 +1,7 @@
 // Author: Max Yi-Hsun Chou <yihsunmaxchou@icloud.com>
 // Github: https://github.com/maxchou415
 
-const blocklist = ['韓國瑜', '韓市長', '韓總', '國瑜', '韓流', '韓粉', '韓導']
+const blocklist = ['韓國瑜', '韓市長', '韓主委', '韓總', '國瑜', '韓流', '韓粉', '韓導', '韓冰', '韓天', '韓清', '非韓不投', '是韓不投']
 const templateHtml = '<div><h1 style="padding: 30px; text-align: center;">草包已被隱藏！</h1></div>'
 
 const contentFromPosts = document.querySelector('#contentArea')
